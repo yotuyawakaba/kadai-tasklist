fcd7b228667c98835c8bd39a70e532c2775557f8
wakaba.yotuya <cltpotjmdt@gmail.com>
2020-02-22 07:12:19 +0000

    content

--- database/migrations/2020_02_19_004444_create_tasks_table.php
+++ database/migrations/2020_02_19_004444_create_tasks_table.php
@@ -15,7 +15,7 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('contnet');
+            $table->string('content');
             $table->timestamps();
         });
     }
