eccb67c5136b5e013614716e41e73e672e2a2a41
wakaba.yotuya <cltpotjmdt@gmail.com>
2020-02-23 07:43:15 +0000

    add status

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -39,8 +39,10 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, ['status' => 'required | max:10',  'content' => 'required | max:191', ]);
         $task = new Task;
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
         
         return redirect('/');
@@ -80,8 +82,10 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, ['status' => 'required | max:10',  'content' => 'required | max:191', ]);
         $task = Task::find($id);
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
         return redirect('/');
     }
