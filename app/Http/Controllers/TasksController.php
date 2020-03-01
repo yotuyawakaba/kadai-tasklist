6aefe2ee1271d9b88d0da6073ca6452459e07958
wakaba.yotuya <cltpotjmdt@gmail.com>
2020-03-01 16:43:58 +0000

    edit redirect

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -75,7 +75,9 @@ class TasksController extends Controller
     public function edit($id)
     {
         $task = Task::find($id);
-        return view('tasks.edit', ['task' => $task, ]);
+        if (\Auth::id() === $task->user_id) {
+        return view('tasks.edit', ['task' => $task, ]); }
+        return redirect('/');
     }
 
     /**
